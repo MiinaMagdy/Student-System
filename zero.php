6caeb5e619ae1edac93953222a6b1c42f7501919
Mina Magdy <71466151+MinaPhoenix@users.noreply.github.com>
2022-08-18 00:10:03 +0200

    Add files via upload

---
+++ zero.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>zero trial</title>
+    <link rel="stylesheet" href="stylezero.css">
+</head>
+<body>
+
+    <!-- --------------- Start Navigation Bar --------------- -->
+
+    <div class="navbar">
+        <div class="container">
+            <div class="brand">
+                <img class="book-logo" src="book.png" alt="book">
+                <h2 class="brand-text">student system</h2>
+            </div>
+            <ul class="links">
+                <li class="active"><a href="#">home</a></li>
+                <li><a href="#">log in</a></li>
+                <li><a href="#">sign up</a></li>
+            </ul>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+
+    <!-- --------------- End Navigation Bar --------------- -->
+
+    <!-- --------------- Start button --------------- -->
+    
+    <div class="container">
+        <div class="welcome">
+            <h1>Welcome to Student System</h1>
+            <p>Sign up to be one of our Students</p>
+            <button class="btn btn-dark">Sign Up</button>
+            <p>Log in and continue your hard work</p>
+            <button class="btn btn-white">Log in</button>
+        </div>
+    </div>
+
+    <!-- --------------- End button --------------- -->
+    
+</body>
+</html>
