6059c8c99f3154df3ae2b0f62ac0a022e8c511e3
lama salah <lamasalah32@gmail.com>
2022-08-19 01:29:21 +0200

    Create admin-profile.php

---
+++ profile-page/admin-profile.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> profile </title>
+    <link rel="stylesheet" href="/home-page/stylezero.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="scripts.js"></script>
+
+</head>
+
+<body>
+
+    <!-- --------------- Start Navigation Bar --------------- -->
+
+    <div class="navbar">
+        <div class="container">
+            <div class="brand">
+                <img class="book-logo" src="/book2.png" alt="book">
+                <h2 class="brand-text"> student system</h2>
+            </div>
+
+            <ul class="links">
+                <li class="active"><a href="/home-page/zero.php"> home </a></li>
+
+                <div class="dropdown">
+                    <img class="img-profile" src="account.png" alt="profile">
+                    <i class="fa fa-caret-down"></i>
+
+                    <div class="content">
+                        <a href="admin-profile.php"> Your profile </a>
+                        <a href="#"> settings </a>
+                        <a href="#"> sign out</a>
+                    </div>
+
+                </div>
+            </ul>
+            
+            <div class="clearfix"></div>
+        </div>
+    </div>
+
+    <!-- --------------- End Navigation Bar --------------- -->
+
+
+    <!-- --------------- start profile body --------------- -->
+
+    <div class="grid-container">
+        <div>
+            <img src = "account.png" , alt="profile" class="account-pic">
+        </div>
+        <div>
+            <h1> Lama Salah Ahmed </h1> 
+            <ul class="del">
+                <li> <img src = "mail.png" , alt="mail" class="details"> </li>
+                <div class="align"> lamasalah32@gmail.com </div>
+                <li><img src = "phone.png" , alt="phone" class="details"> </li>
+                <div class="align"> 01023839812 </div> 
+            </ul>
+
+            <button class="btn btn-dark"> Edit profile </button>
+        </div>
+    </div>
+
+    
+    <!-- --------------- start tabs --------------- -->
+
+    <div class="tab">
+        <button class="tablinks tab-border" onclick="openTabs(event, 'Student-table')"> Student table </button>
+        <button class="tablinks tab-border" onclick="openTabs(event, 'Edit-student-table')"> Edit student table </button>
+        <button class="tablinks tab-border" onclick="openTabs(event, 'reqests')"> Reqests </button>
+    </div>
+
+    <div id="Student-table" class="tabcontent">
+        <p>Student table</p>
+    </div>
+
+    <div id="Edit-student-table" class="tabcontent">
+        <p>Edit Student table</p> 
+    </div>
+
+    <div id="reqests" class="tabcontent">
+        <p>Reqests</p>
+    </div>
+    
+    <!-- --------------- End tabs --------------- -->
+
+</body>
+</html>
+
