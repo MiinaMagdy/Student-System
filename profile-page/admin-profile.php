392daa49b631200bae3fc96344e4d3c9cde61742
Mina Magdy <71466151+MinaPhoenix@users.noreply.github.com>
2022-08-20 17:13:34 +0200

    Update admin-profile.php
    change dir

--- profile-page/admin-profile.php
+++ profile-page/admin-profile.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> profile </title>
     
-    <link rel="stylesheet" href="/home-page/stylezero.css">
+    <link rel="stylesheet" href="../home-page/stylezero.css">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
@@ -21,7 +21,7 @@
     <div class="navbar">
         <div class="container">
             <div class="brand">
-                <img class="book-logo" src="/book2.png" alt="book">
+                <img class="book-logo" src="../book2.png" alt="book">
                 <h2 class="brand-text"> student system</h2>
             </div>
 
@@ -92,4 +92,3 @@
 
 </body>
 </html>
-
